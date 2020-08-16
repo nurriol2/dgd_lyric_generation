d8c505a0057e15e5948c006656164cf54bd61280
nurriol2 <nurriol2@illinois.edu>
2020-08-16 04:18:44 -0500

    Filter both instrumental and cover albums
    Previously, the filter only caught instrumental tracks.
    Tree City Sessions and some slightly renamed tracks from
    Acceptance Speech 2.0 needed to be filtered.
    
    Each of these cases was consolidated into a single block to
    improve readability.

--- filter_tracks.py
+++ filter_tracks.py
@@ -8,14 +8,16 @@ filename = "~/Desktop/projects/dgd_lyric_generation/tracklist.csv"
 track_df = pd.read_csv(filename)
 
 #remove duplicate tracks
-#track_df = track_df.drop_duplicates(subset="Tracks").reindex([i for i in range(0, len(track_df))])
 track_df = track_df.drop_duplicates(subset="Tracks", ignore_index=True).reset_index()
 
-#remove instrumental tracks
 titles = track_df["Tracks"]
+ignore_indicators = ["Instrumental", "Tree City Sessions", "II"]
 for e, t in enumerate(titles):
-    if "Instrumental" in t:
-        track_df = track_df.drop(e)
+    #remove instrumental tracks and cover albums
+    for indc in ignore_indicators:
+        if indc in t:
+            track_df = track_df.drop(e)
+
 track_df = track_df.reset_index()
 del track_df["level_0"]
 del track_df["index"]
