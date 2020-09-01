94176a9340df6448500908b4581dbee1ffff8109
nurriol2 <nurriol2@illinois.edu>
2020-09-01 14:56:10 -0500

    Fix index to character mapping error

--- webapp/app.py
+++ webapp/app.py
@@ -3,16 +3,19 @@
 import flask
 from flask import Flask, render_template
 from keras.models import load_model
+import tensorflow as tf
 
 #use keras to load the model
 model = load_model("model/dgd_lyric_gen.h5", compile=False)
 
 #character mappings from Dance Gavin Dance lyrics dataset
 char2idx = {'\n': 0, ' ': 1, '!': 2, '"': 3, '&': 4, "'": 5, '(': 6, ')': 7, '+': 8, ',': 9, '-': 10, '.': 11, '0': 12, '1': 13, '2': 14, '3': 15, '4': 16, '5': 17, '6': 18, '7': 19, '8': 20, '9': 21, ':': 22, ';': 23, '?': 24, '[': 25, ']': 26, 'a': 27, 'b': 28, 'c': 29, 'd': 30, 'e': 31, 'f': 32, 'g': 33, 'h': 34, 'i': 35, 'j': 36, 'k': 37, 'l': 38, 'm': 39, 'n': 40, 'o': 41, 'p': 42, 'q': 43, 'r': 44, 's': 45, 't': 46, 'u': 47, 'v': 48, 'w': 49, 'x': 50, 'y': 51, 'z': 52, 'ç': 53, 'é': 54, 'í': 55, 'ú': 56, '\u2005': 57, '\u200a': 58, '’': 59, '‚': 60, '“': 61, '\u205f': 62}
-idx2char = ['\n' ' ' '!' '"' '&' "'" '(' ')' '+' ',' '-' '.' '0' '1' '2' '3' '4' '5'
- '6' '7' '8' '9' ':' ';' '?' '[' ']' 'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i'
- 'j' 'k' 'l' 'm' 'n' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z' 'ç'
- 'é' 'í' 'ú' '\u2005' '\u200a' '’' '‚' '“' '\u205f']
+idx2char = ['\n', ' ', '!', '"', '&', "'", '(', ')', '+', ',', '-', '.', '0',
+       '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '?', '[',
+       ']', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+       'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+       'z', 'ç', 'é', 'í', 'ú', '\u2005', '\u200a', '’', '‚', '“',
+       '\u205f']
 
 def generate_text(model, start_string):
     """
@@ -58,22 +61,32 @@ def generate_text(model, start_string):
 
         text_generated.append(idx2char[predicted_id])
 
-    return (start_string + ''.join(text_generated))
+    output_sequence = start_string + ''.join(text_generated)
+    return output_sequence
+
+def foobar(seq):
+    return seq+"PINEAPPLES"
 
 app = Flask(__name__, template_folder="templates")
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def main():
     if flask.request.method=="GET":
         return render_template("main.html")
 
+    
     if flask.request.method=="POST":
-        sequence_in = flask.request.form["sequence_in"]
-        sequence_in = sequence_in.lower()
-        prediction = generate_text(model, sequence_in)
-        return flask.render_template("main.html", original_input={"input_sequence":sequence_in}, result=prediction)
-
-
+        seq_in = flask.request.form["input_sequence"]
+        seq_in = seq_in.lower()
+        prediction = generate_text(model, seq_in)
+        return flask.render_template("main.html", original_input={"input_sequence":seq_in}, result=prediction)
+    """
+    if flask.request.method=="POST":
+        seq_in = flask.request.form["input_sequence"]
+        seq_in = seq_in.lower()
+        prediction = foobar(seq_in)
+    return flask.render_template("main.html", original_input={"input_sequence":seq_in}, result=prediction)
+    """
 #added so app can be run using terminal like normal module
 if __name__=="__main__":
     app.run()
